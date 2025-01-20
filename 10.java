b6746fea55d574d54b9cb213bbac0eada011b6b5
pooja <pooja.gla_cs21@gla.ac.in>
2025-01-20 22:11:19 +0530

    file added

---
+++ 10.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class KilometerToMileConverter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the distance in kilometers: ");
+        double kilometers = sc.nextDouble();
+        double miles = kilometers * 0.621371;
+        System.out.println("The distance in miles is: " + miles);
+
+    }
+}
---
+++ 2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Add{
+	public static void main(String args[]{
+	Scanner c = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	int c;
+	c = a+b;
+		System.out.println(c);
+		}
+		
+		}
